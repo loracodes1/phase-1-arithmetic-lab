88f2703276084e52e3576d823c09ad7e2b46450a
florence <loracodes3@gmail.com>
2024-10-01 23:05:40 +0300

    vgeh

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
-
 // Write your code here
+let num1 = 2;
+let num2 = 31;
+let multiply = num1 * num2
+console.log(multiply);
+
+let random = Math.floor(Math.random() *10) + 1;
+console.log(random);
+
+let num3 = 39 ; 
+let num4 = 5 ;
+let mod = num3 % num4; 
+console.log(mod);
+
+let numbers = [2,4,6,8,10,12,14,16,18,20];
+let max = Math.max ( ...numbers);
+console.log(max);
